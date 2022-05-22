7c39c43c140c34a442e4ea46cc9de9b26b9323a2
Mikke Schirén <mikke.schiren@digitalistgroup.com>
2022-05-22 18:55:14 +0200

    add UI for invalidated archives

--- Controller.php
+++ Controller.php
@@ -9,13 +9,23 @@
 
 namespace Piwik\Plugins\ExtraTools;
 
-use Piwik\View;
+use Piwik\Plugins\ExtraTools\Lib\Archivers;
+use Piwik\Common;
+use Piwik\Db;
 
 /**
  *
  */
 class Controller extends \Piwik\Plugin\Controller
 {
+
+    private static $rawPrefix = 'segment';
+
+    protected function getTable()
+    {
+        return Common::prefixTable(self::$rawPrefix);
+    }
+
     public function index()
     {
         $api = new API();
@@ -24,5 +34,61 @@ class Controller extends \Piwik\Plugin\Controller
         return $this->renderTemplate('index', array(
             'info' => $info
         ));
+    }
+    public function invalidatedarchives()
+    {
+    //    $result[] = '';
+        $archivers = $this->getArchivers();
+        foreach ($archivers as $out) {
+            if ($out['name'] == 'done') {
+                $out['name'] = 'All visits';
+            } else {
+                $hash = preg_replace('/^done/', '', $out['name']);
+                $name = $this->getSegmentName($hash);
+                $out['name'] = $name['name'];
+            }
+            switch ($out['period']) {
+                case 1:
+                    $out['period'] = 'day';
+                    break;
+                case 2:
+                    $out['period'] = 'week';
+                    break;
+                case 3:
+                    $out['period'] = 'month';
+                    break;
+                case 4:
+                    $out['period'] = 'year';
+                    break;
+            }
+
+            $result[] = $out;
+        }
+        return $this->renderTemplate('invalidatedarchives', array(
+            'archivers' => $result
+        ));
+    }
+
+
+    public function getArchivers()
+    {
+        $list = new Archivers();
+        $out = $list->getAllInvalidations();
+        return $out;
+    }
+
+    public function getSegmentName($hash)
+    {
+        try {
+            $sql = "SELECT `name` FROM " . $this->getTable() . " WHERE `hash` = '" . $hash . "';";
+            $name = $this->getDb()->fetchRow($sql);
+            return $name;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+    private function getDb()
+    {
+        return Db::get();
     }
 }
--- Menu.php
+++ Menu.php
@@ -10,7 +10,11 @@ class Menu extends \Piwik\Plugin\Menu
     public function configureAdminMenu(MenuAdmin $menu)
     {
         if (Piwik::hasUserSuperUserAccess()) {
-            $menu->addDiagnosticItem('ExtraTools_PhpInfo', $this->urlForDefaultAction($orderId = 50));
+            $menu->addDiagnosticItem('ExtraTools_PhpInfo', $this->urlForAction('index'), $order = 50);
+        }
+
+        if (Piwik::hasUserSuperUserAccess()) {
+            $menu->addDiagnosticItem('ExtraTools_Invalidations', $this->urlForAction('invalidatedarchives'), $order = 50);
         }
     }
 }
