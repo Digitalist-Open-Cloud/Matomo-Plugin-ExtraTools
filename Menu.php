273a2f0a2b180e55af55f048eeaa692a4965300f
Mikke Schirén <mikke.schiren@digitalist.com>
2024-04-08 16:30:35 +0200

    new format changelog, new UI

--- Menu.php
+++ Menu.php
@@ -10,13 +10,13 @@ class Menu extends \Piwik\Plugin\Menu
     public function configureAdminMenu(MenuAdmin $menu)
     {
         if (Piwik::isUserHasSomeAdminAccess()) {
-            $menu->registerMenuIcon('ExtraTools', 'icon-rocket');
-            $menu->addItem('ExtraTools', null, $this->urlForAction('index'), $order = 50);
-            $menu->addItem('ExtraTools', 'ExtraTools_ExtraTools', $this->urlForAction('index'), $order = 51);
-            $menu->addItem('ExtraTools', 'ExtraTools_Documentation', $this->urlForAction('docs'), $order = 52);
-            $menu->addItem('ExtraTools', 'ExtraTools_PhpInfo', $this->urlForAction('phpinfo'), $order = 53);
+            $menu->registerMenuIcon('ExtraTools_ExtraTools', 'icon-rocket');
+            $menu->addItem('ExtraTools_ExtraTools', null, $this->urlForAction('index'), $order = 50);
+            $menu->addItem('ExtraTools_ExtraTools', 'ExtraTools_ExtraTools', $this->urlForAction('index'), $order = 51);
+            $menu->addItem('ExtraTools_ExtraTools', 'ExtraTools_Documentation', $this->urlForAction('docs'), $order = 52);
+            $menu->addItem('ExtraTools_ExtraTools', 'ExtraTools_PhpInfo', $this->urlForAction('phpinfo'), $order = 53);
             $menu->addItem(
-                'ExtraTools',
+                'ExtraTools_ExtraTools',
                 'ExtraTools_Invalidations',
                 $this->urlForAction('invalidatedarchives'),
                 $order = 54
